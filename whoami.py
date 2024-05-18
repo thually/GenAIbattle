fd316fc1f83952e29672b39ebeb5157bfc5f66d0
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2024-05-18 14:35:15 +0200

    Create whoami.py
    Small app for playin the game with the deployed bot

---
+++ whoami.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "http://158.177.242.92:31534/api/question"
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json",
+}
+
+print("<|assistant|>: Welcome to the game 'Who am I?'! You can start by asking me a yes or no question to guess who I am.")
+while True:
+    prompt = input("<|user|>: ")
+
+    data = {"question": prompt}
+
+    response = requests.post(url, headers=headers, json=data)
+
+    # If you want to print the response
+    print("<|assistant|>: " + response.json()["answer"])
